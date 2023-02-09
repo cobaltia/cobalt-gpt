ec054a5e78fc831d05b6d9997a5f6cb7017a1288
JuanPablo2655 <personal@isidrocasiano.com>
2023-02-09 18:24:13 -0500

    chore: remove secrets lol

--- src/gpt.ts
+++
@@ -1,9 +0,0 @@
-import { ChatGPTAPI } from 'chatgpt';
-
-const api = new ChatGPTAPI({
-	apiKey: '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',
-	debug: true,
-});
-
-const res = await api.sendMessage('Can you tell me the history of WWII');
-console.log(res.text);
