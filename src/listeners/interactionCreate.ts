4bf4ff0fd44bb02c683dd2213497ddf9d7a041b8
JuanPablo2655 <personal@isidrocasiano.com>
2023-03-01 20:03:35 -0500

    fix: working cooldown

--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -1,10 +1,10 @@
-import { DurationFormatter, Time } from '@sapphire/duration';
-import { RateLimitManager } from '@sapphire/ratelimits';
+import { DurationFormatter } from '@sapphire/duration';
 import { EmbedBuilder, type Interaction, WebhookClient } from 'discord.js';
 import { Events } from '../lib/types/Events.js';
 import { sendMessage } from '#lib/gpt';
 import { Listener } from '#lib/structures';
 import { parseWebhooks } from '#root/config';
+import { ratelimit } from '#utils/cooldown';
 import { truncate } from '#utils/utils';
 
 const webhooks = parseWebhooks();
@@ -20,8 +20,6 @@ abstract class InteractionCreateListener extends Listener<typeof Events.Interact
 		console.log(`${interaction.member.user.tag} triggered ${this.name}`);
 		if (!interaction.isChatInputCommand()) return;
 		const formatter = new DurationFormatter();
-		const rateLimitManager = new RateLimitManager(Time.Hour, 50);
-		const ratelimit = rateLimitManager.acquire('global');
 		if (interaction.commandName === 'ask') {
 			if (ratelimit.limited) {
 				await interaction.reply({
