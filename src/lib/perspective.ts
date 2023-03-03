327f25c57877c80ddc92e4c8ad169b74e66a64d4
JuanPablo2655 <personal@isidrocasiano.com>
2023-03-02 21:33:32 -0500

    refactor: log prompt

--- src/lib/perspective.ts
+++ src/lib/perspective.ts
@@ -12,34 +12,48 @@ async function scoreMessage(message: string): Promise<AnalyzeMessageResult> {
 	const result = await perspective.analyze(message, {
 		attributes: ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK', 'INSULT', 'PROFANITY', 'THREAT'],
 	});
+	const TOXICITY = result.attributeScores.TOXICITY.summaryScore.value;
+	const SEVERE_TOXICITY = result.attributeScores.SEVERE_TOXICITY.summaryScore.value;
+	const IDENTITY_ATTACK = result.attributeScores.IDENTITY_ATTACK.summaryScore.value;
+	const INSULT = result.attributeScores.INSULT.summaryScore.value;
+	const PROFANITY = result.attributeScores.PROFANITY.summaryScore.value;
+	const THREAT = result.attributeScores.THREAT.summaryScore.value;
+	const weight = {
+		TOXICITY: 0.9,
+		SEVERE_TOXICITY: 0.7,
+		IDENTITY_ATTACK: 0.7,
+		INSULT: 0.9,
+		PROFANITY: 0.9,
+		THREAT: 0.7,
+	};
 	return {
 		attributes: {
-			TOXICITY: result.attributeScores.TOXICITY.summaryScore.value,
-			SEVERE_TOXICITY: result.attributeScores.SEVERE_TOXICITY.summaryScore.value,
-			IDENTITY_ATTACK: result.attributeScores.IDENTITY_ATTACK.summaryScore.value,
-			INSULT: result.attributeScores.INSULT.summaryScore.value,
-			PROFANITY: result.attributeScores.PROFANITY.summaryScore.value,
-			THREAT: result.attributeScores.THREAT.summaryScore.value,
+			TOXICITY: `${TOXICITY} ${TOXICITY > weight.TOXICITY ? '•' : ''}`,
+			SEVERE_TOXICITY: `${SEVERE_TOXICITY} ${SEVERE_TOXICITY > weight.SEVERE_TOXICITY ? '•' : ''}`,
+			IDENTITY_ATTACK: `${IDENTITY_ATTACK} ${IDENTITY_ATTACK > weight.IDENTITY_ATTACK ? '•' : ''}`,
+			INSULT: `${INSULT} ${INSULT > weight.INSULT ? '•' : ''}`,
+			PROFANITY: `${PROFANITY} ${PROFANITY > weight.PROFANITY ? '•' : ''}`,
+			THREAT: `${THREAT} ${THREAT > weight.THREAT ? '•' : ''}`,
 		},
 		score: [
-			result.attributeScores.TOXICITY.summaryScore.value > 0.9,
-			result.attributeScores.SEVERE_TOXICITY.summaryScore.value > 0.7,
-			result.attributeScores.IDENTITY_ATTACK.summaryScore.value > 0.7,
-			result.attributeScores.INSULT.summaryScore.value > 0.9,
-			result.attributeScores.PROFANITY.summaryScore.value > 0.9,
-			result.attributeScores.THREAT.summaryScore.value > 0.7,
+			TOXICITY > weight.TOXICITY,
+			SEVERE_TOXICITY > weight.SEVERE_TOXICITY,
+			IDENTITY_ATTACK > weight.IDENTITY_ATTACK,
+			INSULT > weight.INSULT,
+			PROFANITY > weight.PROFANITY,
+			THREAT > weight.THREAT,
 		],
 	};
 }
 
 export interface AnalyzeMessageResult {
 	attributes: {
-		TOXICITY: number;
-		SEVERE_TOXICITY: number;
-		IDENTITY_ATTACK: number;
-		INSULT: number;
-		PROFANITY: number;
-		THREAT: number;
+		TOXICITY: string;
+		SEVERE_TOXICITY: string;
+		IDENTITY_ATTACK: string;
+		INSULT: string;
+		PROFANITY: string;
+		THREAT: string;
 	};
 	score: boolean[];
 }
