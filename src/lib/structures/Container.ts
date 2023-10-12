5dfec430303a9e6fb9835e5e5e6f63e11ebd3f50
JuanPablo2655 <personal@isidrocasiano.com>
2023-10-12 17:19:10 -0400

    feat: command structure

--- src/lib/structures/Container.ts
+++ src/lib/structures/Container.ts
@@ -1,11 +1,13 @@
 import { Collection } from 'discord.js';
-import type { Listener } from '#lib/structures';
+import type { GenericCommand, Listener } from '#lib/structures';
 
 export interface Container {
 	listeners: Collection<string, Listener>;
+	commands: Collection<string, GenericCommand>;
 }
 
 // @ts-expect-error: injected variables not initiated yet
 export const container: Container = {
 	listeners: new Collection<string, Listener>(),
+	commands: new Collection<string, GenericCommand>(),
 };
